9c0718c353cc4fb2c521f1ce6438acb5bd70ea74
robmellett <robmellett@gmail.com>
2019-03-07 16:20:43 +1100

    Ready for testing

--- build.sh
+++ build.sh
@@ -2,6 +2,18 @@
 
 versions=('php7.3' 'php7.2' 'php7.1' 'php7.0' 'php5.6')
 
-for i in "${versions[@]}"; do
-  echo $i
+for version in "${versions[@]}"; do
+
+  oldstring="php7.2"
+  newstring=$version
+
+  echo ">>> Creating docker image for: $version"
+
+  find . -type f \( ! -path '*/.git/*' -and ! -path 'build.sh' \) \
+    -exec sed -i 's/$oldstring/$newstring/g' {} \;
+
+  exit
+
+  echo "\n"
+
 done
