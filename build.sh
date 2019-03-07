af284a82f764ad9e91c3fd9f7350a472cb4f33a7
robmellett <robmellett@gmail.com>
2019-03-07 16:38:46 +1100

    Overwrite docker-compose

--- build.sh
+++ build.sh
@@ -4,16 +4,13 @@ versions=('php7.3' 'php7.2' 'php7.1' 'php7.0' 'php5.6')
 
 for version in "${versions[@]}"; do
 
-  oldstring="php7.2"
+  oldstring="php7.2" # What is currently in the file
   newstring=$version
 
   echo ">>> Creating docker image for: $version"
 
-  find . -type f \( ! -path '*/.git/*' -and ! -path 'build.sh' \) \
-    -exec sed -i 's/$oldstring/$newstring/g' {} \;
+  sed 's/$oldstring/$newstring/g' 'docker-compose.yaml'
 
-  exit
-
-  echo "\n"
+  echo ""
 
 done
