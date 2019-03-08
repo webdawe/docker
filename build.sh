bfa463cc89886a5b694b55ba4c1c98c818da2b25
robmellett <robmellett@gmail.com>
2019-03-08 14:54:02 +1100

    Refactor

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-versions=('7.3' '7.2' '7.1' '7.0' '5.6')
+versions=('7.3' '7.1' '7.0' '5.6')
 
-oldstring="7.2" # What is currently in the file
+currentversion="7.2" # What is currently in the file
 
 for version in "${versions[@]}"; do
 
-  newstring=$version
   IMAGE_TAG=beyondlimits99/php
 
   echo ">>> Creating docker image for: $version"
 
   # Search and replace PHP Versions in all files
-  grep -rli "$oldstring" * | xargs -i@ sed -i "s/$oldstring/$newstring/g" @
+  grep -rli "$currentversion" * | xargs -i@ sed -i "s/$currentversion/$version/g" @
 
   echo "The image tag is $IMAGE_TAG"
   docker build --pull -t $IMAGE_TAG:$version .
@@ -21,6 +20,6 @@ for version in "${versions[@]}"; do
   docker push $IMAGE_TAG:$version
 
   # Switch to next version
-  oldstring=$version
+  currentversion=$version
 
 done
