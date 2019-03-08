be271d2a39b35e7e7f3c939511643439543cd4ac
robmellett <robmellett@gmail.com>
2019-03-08 11:56:17 +1100

    Update build script

--- build.sh
+++ build.sh
@@ -4,20 +4,23 @@
 # This script is run locally.  Not on Gitlab CI
 #
 
-versions=('php7.3' 'php7.2' 'php7.1' 'php7.0' 'php5.6')
+versions=('7.3' '7.2' '7.1' '7.0' '5.6')
 
 for version in "${versions[@]}"; do
 
-  oldstring="php7.2" # What is currently in the file
+  oldstring="7.2" # What is currently in the file
   newstring=$version
-  IMAGE_TAG=beyondlimits99
+  IMAGE_TAG=beyondlimits99/php
 
   echo ">>> Creating docker image for: $version"
 
   sed "s/$oldstring/$newstring/g" "Dockerfile"
+  sed -i.bak -e "s/7.2/7.3/g" "Dockerfile"
 
   echo "The image tag is $IMAGE_TAG"
   docker build --pull -t $IMAGE_TAG:$version .
+
+  # Push to the Docker Repo
   docker push $IMAGE_TAG:$version
 
 done
