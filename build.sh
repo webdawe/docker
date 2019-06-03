757e738672a1e3b0a9808d592cf2372f22b175c4
robmellett <robmellett@gmail.com>
2019-06-03 11:34:30 +1000

    Add local build step

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Use this to build locally
+docker build --pull -t robmellett/lemp:7.2 .
--- docker.sh
+++ docker.sh
@@ -3,9 +3,9 @@
 # Find container names with `docker container ls`
 
 echo ">> Database IP:"
-echo $(docker inspect docker-xdebug_web_1 | grep IPAddress)
+echo $(docker inspect docker-web | grep IPAddress)
 
 # SSH into ETL App
-docker exec -it --user ubuntu docker-xdebug_web_1 bash
+docker exec -it --user ubuntu docker-web bash
 
-docker exec -it docker-xdebug_web_1 bash
+docker exec -it docker-web bash
