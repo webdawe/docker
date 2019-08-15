a88eb0c0b5d2646ad5adacefb78bfe1d5bc96416
robmellett <robmellett@gmail.com>
2019-08-15 16:43:54 +1000

    Build step

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-# Use this to build locally
-docker build --pull -t robmellett/lemp:7.2 .
+# Test Docker has been configured locally
+docker login
+
+# Build CI Images Locally | -f Dockerfile
+docker build -t robmellett/base:latest .
+docker push robmellett/base:latest
+
+# docker build -f Dockerfile.web --pull robmellett/lemp:7.2 .
