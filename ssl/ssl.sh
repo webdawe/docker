4dbfc0c2da4b29097fd598a4b7c0cc9550705b47
robmellett <robmellett@gmail.com>
2019-08-30 22:38:52 +1000

    Fix typo

--- ssl/ssl.sh
+++ ssl/ssl.sh
@@ -6,4 +6,4 @@ openssl req -x509 -out /etc/ssl/certs/nginx-selfsigned.crt -keyout /etc/ssl/priv
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
-openssl dhparam -out /etc/ssl/certs/dhparam.pem; 2048
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
