1efc12d8bfe30a1e278dc871d093f2662d89e0b1
robmellett <robmellett@gmail.com>
2019-08-31 00:13:58 +1000

    Try this

--- ssl/ssl.sh
+++ ssl/ssl.sh
@@ -1,12 +1,16 @@
 ## Documentation
 # https://letsencrypt.org/docs/certificates-for-localhost/
+# https://superuser.com/questions/226192/avoid-password-prompt-for-keys-and-prompts-for-dn-information
 
-# openssl req -x509 -out /etc/ssl/certs/nginx-selfsigned.crt -keyout /etc/ssl/private/nginx-selfsigned.key \
-#   -newkey rsa:2048 -nodes -sha256 \
-#   -subj '/CN=localhost' -extensions EXT -config <( \
-#    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
-openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout /etc/ssl/certs/nginx-selfsigned.crt -out /etc/ssl/certs/nginx-selfsigned.crt -days 365
+openssl req \
+  -new \
+  -newkey rsa:4096 \
+  -days 365 \
+  -nodes \
+  -x509 \
+  -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
+  -keyout /etc/ssl/certs/nginx-selfsigned.crt \
+  -out /etc/ssl/certs/nginx-selfsigned.crt
 
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
