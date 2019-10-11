9d9caf07d8c67cd0587d30802cc0a68ab64c1bb9
Rob Mellett <robmellett@gmail.com>
2019-10-11 16:21:51 +0700

    Add Docker container name

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 # Find container names with `docker container ls`
 
 echo ">> Database IP:"
-echo $(docker inspect docker-web | grep IPAddress)
+echo $(docker inspect docker-mysql | grep IPAddress)
 
 # SSH into Web App
 docker exec -it --user ubuntu docker-web bash
