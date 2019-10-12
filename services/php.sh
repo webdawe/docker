f8e0fcf74031e6e48390cd96d89ebecd300808e0
Rob Mellett <robmellett@gmail.com>
2019-10-13 00:03:45 +0700

    Force ownership

--- services/php.sh
+++ services/php.sh
@@ -15,3 +15,6 @@ sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
 sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/fpm/php.ini
 sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/fpm/php.ini
 sed -i "s/max_execution_time = .*/max_execution_time = 300/" /etc/php/7.3/fpm/php.ini
+
+# Force all files to have correct ownership permissions
+chown ubuntu:www-data -R /var/www/html
