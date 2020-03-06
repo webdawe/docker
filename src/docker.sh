4caef85400e54811ab6988edfc26a93d58cf67e5
Rob <dev@robmellett.com>
2020-03-07 01:38:54 +1000

    Override SSL certs in app container

--- src/docker.sh
+++ src/docker.sh
@@ -3,7 +3,7 @@
 # Find container names with `docker container ls`
 
 echo ">> Database IP:"
-echo $(docker inspect docker-mysql | grep IPAddress)
+echo $(docker inspect docker-database | grep IPAddress)
 
 # SSH into Web App
 docker exec -it --user ubuntu acme-app bash
